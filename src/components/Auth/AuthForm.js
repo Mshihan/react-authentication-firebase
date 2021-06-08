0bfadb4529bc2cab444dc9c1cae6e88805456e69
Mshihan <167sihan@gmail.com>
2021-06-08 17:28:17 +0530

    finalized

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -61,10 +61,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        const expirationTime = new Date(
-          new Date().getTime() + new Date(+data.expiresIn * 1000)
-        );
-        authCtx.login(data.idToken, expirationTime);
+        authCtx.login(data.idToken);
         history.replace("/");
       })
       .catch((error) => {
